5a31a0f66f0a4ad27521a3cdbb720a2d70912cae
Lê Trọng Tuyên <85683857+tuyenlt@users.noreply.github.com>
2025-04-12 06:08:15 +0000

    code sand box innit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 // App.jsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import About from './pages/About';
-import Blog from './pages/Blog';
-import Navbar from './components/Navbar';
-import { AuthProvider } from './context/AuthProvider';
-import ProtectedRoute from './context/ProtectedRoute';
-import BlogDetail from './pages/BlogDetail';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Navbar from "./components/Navbar";
+import { AuthProvider } from "./context/AuthProvider";
+import ProtectedRoute from "./context/ProtectedRoute";
+import BlogDetail from "./pages/BlogDetail";
 
 const App = () => {
   return (
@@ -18,9 +18,24 @@ const App = () => {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/home" index element={<Home />} />
-          <Route path="/blog" element={<ProtectedRoute> <Blog /> </ProtectedRoute>} />
-          <Route path="/blog/:id" element={<ProtectedRoute> <BlogDetail /> </ProtectedRoute>} />
+          <Route
+            path="/blog"
+            element={
+              <ProtectedRoute>
+                <Blog />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/blog/:id"
+            element={
+              <ProtectedRoute>
+                <BlogDetail />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/about" element={<About />} />
+          <Route path="*" element={<Home />} />
         </Routes>
       </AuthProvider>
     </Router>
